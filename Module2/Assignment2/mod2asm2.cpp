ea983b3b095af1e434a47bb0b87c3449eefeb44b
Shaun Carter <why06mail@gmail.com>
2015-04-10 02:36:07 +0000

    fixed Module 2 Assignment 2

--- Module2/Assignment2/mod2asm2.cpp
+++ Module2/Assignment2/mod2asm2.cpp
@@ -14,35 +14,46 @@
 #define CANDYBARCOST 10
 #define GUMBALLCOST 3
 //Functions
-void calcCoupons(int);
+void calcCoupons(int, int*, int*, int*);
 
 using namespace std;
 int main()
 {
-    //Input # of coupons
+    //variables
     int coupons = 0;
+    int candybars, gumballs, leftover = 0;
+    
+    //Input # of coupons
     cout << "Please enter number of coupons: ";
     cin >> coupons;
+    
+    if(coupons < 0){
+        cout<<"\nSorry. This program positive integer values. (Ex. 1,2,3....)\n";
+        return 0;
+    }
+
+    
     //Calculate prizes
-    calcCoupons(coupons);
+    calcCoupons(coupons, &candybars, &gumballs, &leftover);
+    
+    //print prizes
+    cout << "\nNumber of Candybars = " << candybars << "\n\n";
+    cout << "Number of Gumballs = " <<  gumballs << "\n\n";
+    cout << "Remaining coupons = " << leftover <<"\n";
     return 0;
 }
 
 //prints our result of coupon calculation preferring candybars
-void calcCoupons(int coupons){
-    //how many candy bars
-    if(coupons >=10){        
-        cout << "\nNumber of Candy bar = " << coupons / CANDYBARCOST << "\n\n";
-        //call again with remainder
-        calcCoupons(coupons % CANDYBARCOST);
-    //how many gumballs
-    }else if(coupons >= 3){
-        cout << "Number of Gumballs = " << coupons / GUMBALLCOST  << "\n\n";
-        //call again with remainder
-        calcCoupons(coupons % GUMBALLCOST);
+void calcCoupons(int coupons, int* candybars, int* gumballs, int* leftover){
+    //candy bars
+    *candybars = coupons / CANDYBARCOST;
+    coupons = coupons % CANDYBARCOST;
+    //gumballs
+    *gumballs = coupons / GUMBALLCOST;
+    coupons = coupons % GUMBALLCOST;
     //leftover coupons
-    }else{
-        cout << "Remaining coupons = " << coupons <<"\n";
-        return;
-    }   
+    *leftover = coupons;
+    return;
 }
+
+//NOTE: One day I will find a good situation to use recursion.
